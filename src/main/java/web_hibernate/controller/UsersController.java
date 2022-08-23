54a35afb4ebaf7a6d555cbe67b6e5c00d0f94ed7
Maxim21S <maximsavin21@gmail.com>
2022-08-24 02:50:02 +0300

    changed annotations in UserController to Get/PostMapping

--- src/main/java/web_hibernate/controller/UsersController.java
+++ src/main/java/web_hibernate/controller/UsersController.java
@@ -3,9 +3,7 @@ package web_hibernate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web_hibernate.model.User;
 import web_hibernate.service.UserService;
 
@@ -17,34 +15,34 @@ public class UsersController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String showAllUsers(Model model) {
         List<User> userList = userService.listUsers();
         model.addAttribute("allUsers", userList);
         return "users";
     }
 
-    @RequestMapping("addUser")
+    @GetMapping("addUser")
     public String addUser(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "user-details";
     }
 
-    @RequestMapping("createUser")
+    @PostMapping("createUser")
     public String createUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
         return "redirect:/";
     }
 
-    @RequestMapping("editUser")
+    @PostMapping("editUser")
     public String editUser(@RequestParam("userID") int id, Model model) {
         User user = userService.getUser(id);
         model.addAttribute("user", user);
         return "user-details";
     }
 
-    @RequestMapping("deleteUser")
+    @PostMapping("deleteUser")
     public String deleteUser(@RequestParam("userID") int id) {
         userService.deleteUser(id);
         return "redirect:/";
